e20b8ed6af128016241cdf80630589dc5cb57124
FongMi <b8892778@gmail.com>
2025-07-01 14:00:40 +0800

    Add item vac when move player

--- src/main/java/net/server/channel/handlers/MovePlayerHandler.java
+++ src/main/java/net/server/channel/handlers/MovePlayerHandler.java
@@ -21,28 +21,62 @@
 */
 package net.server.channel.handlers;
 
+import client.Character;
 import client.Client;
 import net.packet.InPacket;
+import server.maps.MapItem;
+import server.maps.MapObject;
+import server.maps.MapObjectType;
 import tools.PacketCreator;
 import tools.exceptions.EmptyMovementException;
 
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public final class MovePlayerHandler extends AbstractMovementPacketHandler {
+
+    private static final Map<Integer, Long> lastVacTimeMap = new ConcurrentHashMap<>();
+    private static final long VAC_INTERVAL_MS = 3000;
+
     @Override
-    public final void handlePacket(InPacket p, Client c) {
+    public void handlePacket(InPacket p, Client c) {
         p.skip(9);
         try {   // thanks Sa for noticing empty movement sequences crashing players
             int movementDataStart = p.getPosition();
             updatePosition(p, c.getPlayer(), 0);
             long movementDataLength = p.getPosition() - movementDataStart; //how many bytes were read by updatePosition
             p.seek(movementDataStart);
-
             c.getPlayer().getMap().movePlayer(c.getPlayer(), c.getPlayer().getPosition());
             if (c.getPlayer().isHidden()) {
                 c.getPlayer().getMap().broadcastGMMessage(c.getPlayer(), PacketCreator.movePlayer(c.getPlayer().getId(), p, movementDataLength), false);
             } else {
                 c.getPlayer().getMap().broadcastMessage(c.getPlayer(), PacketCreator.movePlayer(c.getPlayer().getId(), p, movementDataLength), false);
             }
+            tryVac(c);
         } catch (EmptyMovementException e) {
+        }
+    }
+
+    private void tryVac(Client c) {
+        int playerId = c.getPlayer().getId();
+        long now = System.currentTimeMillis();
+        long lastVacTime = lastVacTimeMap.getOrDefault(playerId, 0L);
+        if (now - lastVacTime >= VAC_INTERVAL_MS) {
+            itemVac(c);
+            lastVacTimeMap.put(playerId, now);
+        }
+    }
+
+    private void itemVac(Client c) {
+        Character player = c.getPlayer();
+        List<MapObject> items = player.getMap().getMapObjectsInRange(player.getPosition(), Double.POSITIVE_INFINITY, List.of(MapObjectType.ITEM));
+        for (MapObject item : items) {
+            MapItem mapItem = (MapItem) item;
+            if (!mapItem.canBePickedBy(player)) continue;
+            if (mapItem.getOwnerId() == player.getId() || mapItem.getOwnerId() == player.getPartyId()) {
+                player.pickupItem(mapItem);
+            }
         }
     }
 }
