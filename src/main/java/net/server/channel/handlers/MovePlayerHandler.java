e4ec3ad3c43cf03171f7947fd2e2157933dc26f7
FongMi <b8892778@gmail.com>
2025-07-04 06:41:09 +0800

    Revert vac item

--- src/main/java/net/server/channel/handlers/MovePlayerHandler.java
+++ src/main/java/net/server/channel/handlers/MovePlayerHandler.java
@@ -21,23 +21,12 @@
 */
 package net.server.channel.handlers;
 
-import client.Character;
 import client.Client;
 import net.packet.InPacket;
-import server.maps.MapItem;
-import server.maps.MapObject;
-import server.maps.MapObjectType;
 import tools.PacketCreator;
 import tools.exceptions.EmptyMovementException;
 
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
 public final class MovePlayerHandler extends AbstractMovementPacketHandler {
-
-    private static final Map<Integer, Long> lastVacTimeMap = new ConcurrentHashMap<>();
-    private static final long VAC_INTERVAL_MS = 3000;
 
     @Override
     public void handlePacket(InPacket p, Client c) {
@@ -53,30 +42,7 @@ public final class MovePlayerHandler extends AbstractMovementPacketHandler {
             } else {
                 c.getPlayer().getMap().broadcastMessage(c.getPlayer(), PacketCreator.movePlayer(c.getPlayer().getId(), p, movementDataLength), false);
             }
-            tryVac(c);
         } catch (EmptyMovementException e) {
-        }
-    }
-
-    private void tryVac(Client c) {
-        int playerId = c.getPlayer().getId();
-        long now = System.currentTimeMillis();
-        long lastVacTime = lastVacTimeMap.getOrDefault(playerId, 0L);
-        if (now - lastVacTime >= VAC_INTERVAL_MS) {
-            itemVac(c);
-            lastVacTimeMap.put(playerId, now);
-        }
-    }
-
-    private void itemVac(Client c) {
-        Character player = c.getPlayer();
-        List<MapObject> items = player.getMap().getMapObjectsInRange(player.getPosition(), Double.POSITIVE_INFINITY, List.of(MapObjectType.ITEM));
-        for (MapObject item : items) {
-            MapItem mapItem = (MapItem) item;
-            if (!mapItem.canBePickedBy(player)) continue;
-            if (mapItem.getOwnerId() == player.getId() || mapItem.getOwnerId() == player.getPartyId()) {
-                player.pickupItem(mapItem);
-            }
         }
     }
 }
